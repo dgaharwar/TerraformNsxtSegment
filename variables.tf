93fb4ab324ab8387d1055adc9d4deeaf85608617
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-03-19 13:18:39 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,16 +7,17 @@ variable "nsx_server" {}
 variable "network" {
   type = map
   default = {
-    name     = "myTfSegment"
-    cidr = "10.7.99.0/24"
-    transportZone = "N1_TZ_nested_nsx-overlay"
-    tier1 = "myTftier1"
-    networkRangeBegin = "11" # for NSX-T segment if DHCP enabled
-    networkRangeEnd = "50" # for NSX-T segment if DHCP enabled
-    type = "V4" # for Avi
-    dhcp_enabled = "false" # for Avi
+    name                       = "myTfSegment"
+    gateway                    = "10.32.23.1"
+    connected_gateway          = "qa-t1-01"
+    cidr                       = "10.32.23.1/24"
+    transportZone              = "db-tz-1216"
+    tier1                      = "qa-t1-01"
+    networkRangeBegin          = "11" # for NSX-T segment if DHCP enabled
+    networkRangeEnd            = "50" # for NSX-T segment if DHCP enabled
+    type                       = "V4" # for Avi
+    dhcp_enabled               = "true" # for Avi
     exclude_discovered_subnets = "true" # for Avi
-    vcenter_dvs = "true" # for Avi
+    vcenter_dvs                = "true" # for Avi
   }
 }
-
