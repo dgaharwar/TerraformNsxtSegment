40f343199d77ca44dd0e8ddc05f297eaea734ed7
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-03-19 13:10:57 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    # Provider source is used for Terraform discovery and installation of
+    # providers. Declare source for all providers required by the module.
+    nsxt = {
+      source  = "vmware/nsxt"
+    }
+  }
+}
+
 provider "nsxt" {
   host                     = var.nsx_server
   username                 = var.nsx_user
